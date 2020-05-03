cd3b3cc0f26a8878320bd999154734a85fbcd3f6
MrFaraday <faradayby@gmail.com>
2020-05-03 11:55:24 +0300

    c3/s6/ossp/lab-1

---
+++ course-3-semester-6/ossp/lab-1/task-1/resource.h
@@ -0,0 +1,24 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется task-1.rc
+//
+#define IDC_MYICON                      2
+#define IDD_TASK1_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDI_TASK1                       107
+#define IDI_SMALL                       108
+#define IDC_TASK1                       109
+#define IDR_MAINFRAME                   128
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
