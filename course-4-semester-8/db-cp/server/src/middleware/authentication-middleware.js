1931753117f819a7b000e8fbe24afd4d9de17904
MrFaraday <faradayby@gmail.com>
2021-05-25 15:12:27 +0300

    Minors

--- course-4-semester-8/db-cp/server/src/middleware/authentication-middleware.js
+++ course-4-semester-8/db-cp/server/src/middleware/authentication-middleware.js
@@ -3,12 +3,14 @@
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = require('../config')
 
+const headerRegExp = /^Bearer (.*)$/
+
 module.exports = function authenticationMiddleware (req, res, next) {
     if (!req.headers.authorization) {
-        res.sendStatus(401)
+        return res.sendStatus(401)
     }
 
-    const [, token] = /^Bearer (.*)$/.exec(req.headers.authorization)
+    const [, token] = headerRegExp.exec(req.headers.authorization)
 
     try {
         jwt.verify(token, JWT_SECRET)
