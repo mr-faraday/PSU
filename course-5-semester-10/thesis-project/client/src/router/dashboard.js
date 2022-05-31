e66e3d682b212098635611ed944104939162c17d
MrFaraday <faradayby@gmail.com>
2022-05-31 23:58:39 +0300

    panel items

--- course-5-semester-10/thesis-project/client/src/router/dashboard.js
+++ course-5-semester-10/thesis-project/client/src/router/dashboard.js
@@ -10,6 +10,33 @@ const dashboardItems = [
     },
     roles: [UserRoleId.ADMIN],
   },
+  {
+    name: 'Клиенты',
+    href: '/dashboard/clients',
+    component: 'ClientsView',
+    meta: {
+      title: 'Клиенты',
+    },
+    roles: [UserRoleId.ADMIN, UserRoleId.MANAGER],
+  },
+  {
+    name: 'Задачи по приёму/распределению',
+    href: '/dashboard/tasks',
+    component: 'TasksView',
+    meta: {
+      title: 'Задачи по приёму/распределению',
+    },
+    roles: [UserRoleId.ADMIN, UserRoleId.MANAGER, UserRoleId.OPERATOR],
+  },
+  {
+    name: 'Схема склада',
+    href: '/dashboard/warehouse-scheme',
+    component: 'WarehouseSchemeView',
+    meta: {
+      title: 'Схема склада',
+    },
+    roles: [UserRoleId.ADMIN, UserRoleId.OPERATOR],
+  },
 ]
 
 export default dashboardItems
